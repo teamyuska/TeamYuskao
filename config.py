d0576117b12b0ce73e75e832b1a07e7ea5537f13
teamyuska <118346503+teamyuska@users.noreply.github.com>
2023-12-21 11:46:25 +0400

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from youtube_dl import YoutubeDL
 
 class Config:
     APP_ID = int(os.environ.get("APP_ID", "7948888"))
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "48285ac512613754f7913b310e884222")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6746110732:AAEqr0qHkncbPANl4sIyY3Zsrz08qg88tkc")
     START_MSG = os.environ.get("START_MSG", "<b>Hi {},\nIam A Simple Youtube to Mp3 Downloader Bot,</b>\n\nSend me Any Songs name with /song command")
     START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2a35fca576aa49de77c98.jpg")
